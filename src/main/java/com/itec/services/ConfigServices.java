cda3e4b3b712134b5625bd298a9815af2281199c
dapadillar <hellomotorola>
2016-06-15 20:43:36 -0500

    new methods

--- src/main/java/com/itec/services/ConfigServices.java
+++ src/main/java/com/itec/services/ConfigServices.java
@@ -20,16 +20,38 @@ import java.util.List;
 
 @Path("/garantias/config")
 @Produces(MediaType.APPLICATION_JSON)
+
+
 public class ConfigServices {
     FactoryMongo fm = new FactoryMongo();
     HashMap<String, String> criterial= new HashMap<>();
+
+
+
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/garantias-field")
     @PermitAll
-    public List<DBObject> getGarantiasFiled() throws IOException {
+    public List<DBObject> getGarantiasFiled(@Context HttpServletRequest req) throws IOException {
         criterial.clear();
-        return fm.getGarantias(criterial);
+        fillCriterialFromString(req.getQueryString());
+        return fm.getGarantiasFields(criterial);
+    }
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/garantias-field")
+    @PermitAll
+    public String removeGarantiasFiled(@Context HttpServletRequest req) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String read;
+        while((read=br.readLine()) != null) {
+            stringBuilder.append(read);
+        }
+        br.close();
+        fm.deleteGarantiasFields(stringBuilder.toString());
+        return "Elimiando";
     }
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -45,5 +67,62 @@ public class ConfigServices {
         br.close();
         fm.insertGarantias(stringBuilder.toString());
         return  "FIRMANDO";
+    }
+
+
+
+    /*PARAMETRICS VALUES*/
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/garantias-parametricvalues")
+    @PermitAll
+    public List<DBObject> getGarantiasParametricValues(@Context HttpServletRequest req) throws IOException {
+        criterial.clear();
+        fillCriterialFromString(req.getQueryString());
+        return fm.getGarantiasParametricValues(criterial);
+    }
+
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/garantias-parametricvalues")
+    @PermitAll
+    public String removeGarantiasParametricValues(@Context HttpServletRequest req) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String read;
+        while((read=br.readLine()) != null) {
+            stringBuilder.append(read);
+        }
+        br.close();
+        fm.deleteParametricValues(stringBuilder.toString());
+        return "Elimiando";
+    }
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/garantias-parametricvalues")
+    @PermitAll
+    public String insertGarantiasParametricValues(@Context HttpServletRequest req) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String read;
+        while((read=br.readLine()) != null) {
+            stringBuilder.append(read);
+        }
+        br.close();
+        fm.insertGarantiasParametricValues(stringBuilder.toString());
+        return  "FIRMANDO";
+    }
+
+
+
+    /*OTHER METHOD*/
+    private void fillCriterialFromString( String queryString){
+        criterial.clear();
+        if(queryString!=null)
+            for (String split : queryString.split("&")) {
+                if (split.split("=").length == 2) {
+                    criterial.put(split.split("=")[0], split.split("=")[1]);
+                }
+            }
     }
 }
